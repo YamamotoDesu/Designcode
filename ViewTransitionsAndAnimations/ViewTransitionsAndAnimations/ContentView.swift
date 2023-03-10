5f836d53a027c7d686d03b2235b29fd622996038
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-10 12:29:26 +0900

    View Transition

---
+++ ViewTransitionsAndAnimations/ViewTransitionsAndAnimations/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  ViewTransitionsAndAnimations
+//
+//  Created by 山本響 on 2023/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var show = false
+
+    var body: some View {
+        ZStack {
+            Text("View Transaction")
+                .padding()
+                .background(Capsule().stroke())
+            if show {
+                RoundedRectangle(cornerRadius: 30)
+                    .fill(Color.blue)
+                    .padding()
+                    .transition(.slide)
+                    .zIndex(1)
+            }
+        }
+        .onTapGesture {
+            withAnimation(.spring()) {
+                show.toggle()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewTransitionsAndAnimations/ViewTransitionsAndAnimations/ViewTransitionsAndAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewTransitionsAndAnimationsApp.swift
+//  ViewTransitionsAndAnimations
+//
+//  Created by 山本響 on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct ViewTransitionsAndAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
