0407d621a73636221cf6ee91b3a045e33379f3b6
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-10 15:51:54 +0900

    Transform Animations

---
+++ TransformAnimations/TransformAnimations/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  TransformAnimations
+//
+//  Created by 山本響 on 2023/03/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var show = false
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 260, height: 200)
+                .offset(y: 20)
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 300, height: 200)
+                .foregroundColor(Color.purple)
+                .offset(y: show ? -200 : 0)
+                .scaleEffect(show ? 1.2 : 1)
+                .rotationEffect(Angle(degrees: show ? 30 : 0))
+                .rotation3DEffect(
+                    Angle(degrees: show ? 30 : 0),
+                    axis: (x: 1, y: 0, z: 0),
+                    anchor: .center,
+                    anchorZ: 0.0,
+                    perspective: 1
+                )
+                .onTapGesture {
+                    show.toggle()
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TransformAnimations/TransformAnimations/TransformAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransformAnimationsApp.swift
+//  TransformAnimations
+//
+//  Created by 山本響 on 2023/03/10.
+//
+
+import SwiftUI
+
+@main
+struct TransformAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
